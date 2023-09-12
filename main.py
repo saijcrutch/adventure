81153986609bd92e2187e70865bddf30dd3154ce
SilonCrutch <cfielda@gmail.com>
2023-09-12 18:53:30 -0500

    created new file

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import choices
+# Story of a man trying to get $100 to take a trip
+
+name = input("What do you want your character's name to be? ")
+intro = print(f"This story is about you, {name}, and your quest to get $100 to go on vacation.")
+
+def adventure():
+    
